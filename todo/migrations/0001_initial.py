50a5fa436b894b7b8f2bb6d0fb9a7ba7de819718
randyaajr <randyaajrsp@gmail.com>
2021-01-12 00:33:22 +0000

    Cleaded up(pep 8)

--- todo/migrations/0001_initial.py
+++ todo/migrations/0001_initial.py
@@ -14,7 +14,11 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Item',
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('id', models.AutoField(
+                    auto_created=True,
+                    primary_key=True,
+                    serialize=False,
+                    verbose_name='ID')),
                 ('name', models.CharField(max_length=50)),
                 ('done', models.BooleanField(default=False)),
             ],
