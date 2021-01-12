c13eaff6849a15ab5d1215ae482eb0c8ae042e75
randyaajr <randyaajrsp@gmail.com>
2021-01-12 00:43:47 +0000

    Dictionary added

--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from .models import Item
 
 
 def get_todo_list(request):
-    return render(request, 'todo/todo_list.html')
+    items = Item.objects.all()
+    context = {
+        'items': items
+    }
+    return render(request, 'todo/todo_list.html', context)
