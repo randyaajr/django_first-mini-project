8ee8ec869aaf2c879c4bc3c0149ca46913b3d97a
randyaajr <randyaajrsp@gmail.com>
2021-01-19 22:33:45 +0000

    Migrated data to heroku database

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -74,11 +75,15 @@ WSGI_APPLICATION = 'django_todo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse('postgres://yfhtfnktsjuguh:aeafdcd37ad86f622f2496ed6ecdbc6221ad1f9e172396773d8fb5c827a6cdd0@ec2-54-237-135-248.compute-1.amazonaws.com:5432/d8bdujn5lv2ujk')
 }
 
 
